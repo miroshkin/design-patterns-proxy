a2fe063f0cadf6a16e8c1318b419ff1e1c5737c7
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-16 12:10:52 +0300

    Added project

---
+++ DesignPatterns_Proxy/DesignPatterns_Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Proxy!");
+        }
+    }
+}
